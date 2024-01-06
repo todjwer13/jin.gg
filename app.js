6f67bd1f0ce193eb22a38c906f47adcd3efa1c50
todjwer13 <todjwr12@gmail.com>
2024-01-06 14:09:43 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', []);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+});
