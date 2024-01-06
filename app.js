b5d48c0077542972bd1dac1b6b18773e32c38ecd
todjwer13 <todjwr12@gmail.com>
2024-01-06 14:26:21 +0900

    fix: 오류 해결

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/', []);
+// app.use('/api', []);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
