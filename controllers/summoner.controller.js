8d7e000f8741a622ddcf6712111d96ed6e899892
todjwer13 <todjwr12@gmail.com>
2024-01-09 07:39:46 +0900

    feat: 소환사 정보 조회 기능

--- controllers/summoner.controller.js
+++ controllers/summoner.controller.js
@@ -9,8 +9,45 @@ class SummonerController {
       if (!summoner_name || !summoner_tag_line) {
         return res.status(400).json({ Message: error.message });
       }
-      const summoner = await this.summonerService.getSummonerData(summoner_name, summoner_tag_line);
-      return res.status(200).send({ message: 'summoner data:', data: summoner });
+      // 소환사의 puuid 조회
+      const summonerAccount = await this.summonerService.getSummonerPuuid(summoner_name, summoner_tag_line);
+
+      // 조회한 puuid로 소환사 정보 조회
+      const summoner = await this.summonerService.getSummonerId(summonerAccount.puuid);
+      // 소환사 id로 리그 정보 조회
+      const summonerLeague = await this.summonerService.getSummonerLeague(summoner.id);
+
+      // 리그 정보 레코드를 솔로랭크와 자유랭크로 분리
+      const flexLeague = summonerLeague.find((record) => record.queueType === 'RANKED_FLEX_SR');
+      const soloLeague = summonerLeague.find((record) => record.queueType === 'RANKED_SOLO_5x5');
+
+      // 이미 소환사 정보가 DB에 저장되어 있는지 확인
+      const isSummonerExist = await this.summonerService.isSummonerDataExist(summoner.id);
+      if (isSummonerExist) {
+        return res.status(200).send({ message: 'summoner date:', data: isSummonerExist });
+      }
+      // 없다면 소환사 정보 DB 저장
+      const summonerInfo =
+        (summoner.id,
+        summonerAccount.gameName,
+        summonerAccount.tagLine,
+        summoner.summonerLevel,
+        summoner.profileIconId,
+        summoner.puuid,
+        soloLeague.tier,
+        soloLeague.rank,
+        soloLeague.leaguePoints,
+        soloLeague.wins,
+        soloLeague.losses,
+        flexLeague.tier,
+        flexLeague.rank,
+        flexLeague.leaguePoints,
+        flexLeague.wins,
+        flexLeague.losses);
+
+      const summonerDate = await this.summonerService.createSummonerData(summonerInfo);
+
+      return res.status(200).send({ message: 'summoner data:', data: summonerDate });
     } catch (error) {
       console.error('error message:', error.message);
       return res.status(500).send({ message: error.message });
--- services/summoner.service.js
+++ services/summoner.service.js
@@ -2,14 +2,11 @@ const axios = require('axios');
 const { Summoner } = require('../models');
 const apiKey = process.env.RIOT_API_KEY;
 
-const RIOT_API_ENDPOINT = 'https://asia.api.riotgames.com';
-
 class SummonerService {
-  getSummonerData = async (summoner_name, summoner_tag_line) => {
-    const gameName = summoner_name;
-    const tagLine = summoner_tag_line;
+  // 소환사의 puuid 조회
+  getSummonerPuuid = async (summoner_name, summoner_tag_line) => {
     try {
-      const response = await axios.get(`${RIOT_API_ENDPOINT}/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`, {
+      const response = await axios.get(`https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${summoner_name}/${summoner_tag_line}`, {
         headers: {
           'X-Riot-Token': apiKey,
         },
@@ -19,6 +16,50 @@ class SummonerService {
       console.error('error message:', error.response.data);
       throw error;
     }
+  };
+
+  // 소환사 정보 조회
+  getSummonerId = async (puuid) => {
+    try {
+      const response = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}`, {
+        headers: {
+          'X-Riot-Token': apiKey,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('error message:', error.response.data);
+      throw error;
+    }
+  };
+
+  // 소환사 id로 리그 정보 조회
+  getSummonerLeague = async (summoner_id) => {
+    try {
+      const response = await axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner_id}`, {
+        headers: {
+          'X-Riot-Token': apiKey,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      console.error('error message:', error.response.data);
+      throw error;
+    }
+  };
+  // DB에서 puuid를 기준으로 소환사 정보 조회
+  isSummonerDataExist = async (summoner_id) => {
+    console.log(summoner_id);
+    const existingSummoner = await Summoner.findOne({ where: { summoner_id: summoner_id } });
+    return existingSummoner;
+  };
+
+  // 소환사 정보 DB저장
+  createSummonerData = async (summonerInfo) => {
+    const createSummonerData = await Summoner.create({
+      summonerInfo,
+    });
+    return createSummonerData;
   };
 }
 
